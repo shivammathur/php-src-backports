d407d8a8735ebf43bee3e6b49fb013b8aa4b6bfc
Jakub Zelenka <bukka@php.net>
2025-04-10 15:15:36 +0200

    Fix GHSA-3cr5-j632-f35r: Null byte in hostnames
    This fixes stream_socket_client() and fsockopen().
    
    Specifically it adds a check to parse_ip_address_ex and it also makes
    sure that the \0 is not ignored in fsockopen() hostname formatting.
    
    (cherry picked from commit cac8f7f1cf4939f55f06b68120040f057682d89c)
    (cherry picked from commit 36150278addd8686a9899559241296094bd57282)

--- main/streams/xp_socket.c
+++ main/streams/xp_socket.c
@@ -580,12 +580,15 @@ static inline char *parse_ip_address_ex(const char *str, size_t str_len, int *po
 	char *colon;
 	char *host = NULL;
 
-#ifdef HAVE_IPV6
-	char *p;
+	if (memchr(str, '\0', str_len)) {
+		*err = strpprintf(0, "The hostname must not contain null bytes");
+		return NULL;
+	}
 
+#ifdef HAVE_IPV6
 	if (*(str) == '[' && str_len > 1) {
 		/* IPV6 notation to specify raw address with port (i.e. [fe80::1]:80) */
-		p = memchr(str + 1, ']', str_len - 2);
+		char *p = memchr(str + 1, ']', str_len - 2);
 		if (!p || *(p + 1) != ':') {
 			if (get_err) {
 				*err = strpprintf(0, "Failed to parse IPv6 address \"%s\"", str);
