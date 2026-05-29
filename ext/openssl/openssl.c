4eeb25a7ae8f91e517b760423b50a5a5ef9e98fb
David Carlier <devnexen@gmail.com>
2026-05-29 21:44:14 +0100

    ext/openssl: openssl_encrypt() zend mm heap overflow on AES-WRAP-PAD mode.
    Fix #22186
    
    close GH-22187
    
    (cherry picked from commit cbc0489126a7682796aad1e5fb4e51de74af162c)
    (cherry picked from commit 95e9851111d249e43948b76663cff1baeb5e758d)
    (cherry picked from commit 2a73e91a9f9136fbbfcc9177573b6af71e3d5dce)
    (cherry picked from commit e058b01e1bd23421a425cffae9f458b0fa8db222)
    (cherry picked from commit 09cccab30d53614bb826e4390ad23ad7451b6d6c)
    (cherry picked from commit f15d1e26160a8175474160907eb6ab7e10090fa0)
    (cherry picked from commit 58c39c2f8402261fd4e8ffd327c37adc53a9c861)

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -5728,6 +5728,17 @@ PHP_FUNCTION(openssl_encrypt)
 	free_iv = php_openssl_validate_iv(&iv, &iv_len, max_iv_len);
 
 	outlen = data_len + EVP_CIPHER_block_size(cipher_type);
+#ifdef EVP_CIPH_WRAP_MODE
+	if ((EVP_CIPHER_mode(cipher_type)) == EVP_CIPH_WRAP_MODE) {
+		/*
+		 * RFC 5649 wrap-with-padding rounds the input up to the block size
+		 * and prepends an integrity block, we reserve one extra block.
+		 * See EVP_EncryptUpdate(3): wrap mode may write up to
+		 * inl + cipher_block_size bytes.
+		 */
+		outlen += EVP_CIPHER_block_size(cipher_type);
+	}
+#endif
 	outbuf = zend_string_alloc(outlen, 0);
 
 	EVP_EncryptInit(cipher_ctx, cipher_type, NULL, NULL);
@@ -5832,6 +5843,17 @@ PHP_FUNCTION(openssl_decrypt)
 	free_iv = php_openssl_validate_iv(&iv, &iv_len, EVP_CIPHER_iv_length(cipher_type));
 
 	outlen = data_len + EVP_CIPHER_block_size(cipher_type);
+#ifdef EVP_CIPH_WRAP_MODE
+	if ((EVP_CIPHER_mode(cipher_type)) == EVP_CIPH_WRAP_MODE) {
+		/*
+		 * RFC 5649 wrap-with-padding rounds the input up to the block size
+		 * and prepends an integrity block, we reserve one extra block.
+		 * See EVP_EncryptUpdate(3): wrap mode may write up to
+		 * inl + cipher_block_size bytes.
+		 */
+		outlen += EVP_CIPHER_block_size(cipher_type);
+	}
+#endif
 	outbuf = zend_string_alloc(outlen, 0);
 
 	EVP_DecryptInit(cipher_ctx, cipher_type, NULL, NULL);
